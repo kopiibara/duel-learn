e6f3d0796f79179425d8d242b48b5a2fe58f82da
kopiibara <jcarljames123@gmail.com>
2025-01-29 16:06:07 +0800

    added reset/forgot ui

--- frontend/src/routes/PrivateRoutes.tsx
+++ frontend/src/routes/PrivateRoutes.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
 
 import DashboardLayout from "../layouts/DashboardLayout";
 import Home from "../pages/dashboard/home/HomePage";
@@ -7,9 +8,14 @@ import YourLibrary from "../pages/dashboard/my-library/MyLibrary";
 import Profile from "../pages/dashboard/profile/ProfilePage";
 import Shop from "../pages/dashboard/shop/ShopPage";
 import BuyPremium from "../pages/dashboard/shop/BuyPremium";
-import CreateStudyMaterial from "../pages/dashboard/study-material/material-create/CreateStudyMaterial";
 
 const PrivateRoutes = () => {
+  const { user } = useUser();
+
+  if (!user) {
+    return <Navigate to="/landing-page" />;
+  }
+
   return (
     <Routes>
       <Route element={<DashboardLayout />}>
@@ -18,9 +24,7 @@ const PrivateRoutes = () => {
         <Route path="my-library" element={<YourLibrary />} />
         <Route path="profile" element={<Profile />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="study-material/create" element={<CreateStudyMaterial />} />
       </Route>
-
       <Route path="/shop/buy-premium-account" element={<BuyPremium />} />
     </Routes>
   );
