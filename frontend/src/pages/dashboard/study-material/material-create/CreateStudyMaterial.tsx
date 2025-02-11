5335ef26491d9c750e6a10d4ec7a7413cf06600e
kopiibara <jcarljames123@gmail.com>
2025-02-12 01:30:00 +0800

    fixes here and there, added transition effect

--- frontend/src/pages/dashboard/study-material/material-create/CreateStudyMaterial.tsx
+++ frontend/src/pages/dashboard/study-material/material-create/CreateStudyMaterial.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import DocumentHead from "../../../../components/DocumentHead";
+import PageTransition from "../../../../styles/PageTransition";
 import {
   Box,
   Stack,
@@ -126,210 +127,214 @@ const CreateStudyMaterial = () => {
   };
 
   return (
-    <Box className="h-screen w-full px-8">
-      <DocumentHead title="Create Study Material" />
-      <Stack spacing={2.5}>
-        {/* Title Input */}
-        <Box className="sticky top-4 z-10">
-          <Stack direction={"row"} spacing={2} className="flex items-center">
-            <TextField
-              id="title"
-              label="Title"
-              variant="standard"
-              value={title} // <-- Bind to state
-              onChange={(e) => setTitle(e.target.value)} // <-- Update state on change
-              sx={{
-                width: "32rem",
+    <PageTransition>
+      <Box className="h-screen w-full px-8">
+        <DocumentHead title="Create Study Material" />
+        <Stack spacing={2.5}>
+          {/* Title Input */}
+          <Box className="sticky top-4 z-10">
+            <Stack direction={"row"} spacing={2} className="flex items-center">
+              <TextField
+                id="title"
+                label="Title"
+                variant="standard"
+                value={title} // <-- Bind to state
+                onChange={(e) => setTitle(e.target.value)} // <-- Update state on change
+                sx={{
+                  width: "32rem",
 
-                "& .MuiInputLabel-root": { color: "#3B354D" },
-                "& .MuiInputLabel-root.Mui-focused": { color: "#381898" },
-                "& .MuiInput-root": {
+                  "& .MuiInputLabel-root": { color: "#3B354D" },
+                  "& .MuiInputLabel-root.Mui-focused": { color: "#381898" },
+                  "& .MuiInput-root": {
+                    color: "#E2DDF3",
+                    fontWeight: 500,
+                    fontSize: "1.3rem",
+                  },
+                  "& .MuiInput-underline:before": {
+                    borderBottomColor: "#3B354D",
+                  },
+                  "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                    borderBottomColor: "#A38CE6",
+                  },
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "#381898",
+                  },
+                }}
+              />
+
+              <Box flexGrow={1} />
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "0.8rem",
+                  padding: "0.4rem 2rem",
+                  display: "flex",
+                  width: "full",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderColor: "#E2DDF3",
                   color: "#E2DDF3",
-                  fontWeight: 500,
-                  fontSize: "1.3rem",
-                },
-                "& .MuiInput-underline:before": {
-                  borderBottomColor: "#3B354D",
-                },
-                "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
-                  borderBottomColor: "#A38CE6",
-                },
-                "& .MuiInput-underline:after": { borderBottomColor: "#381898" },
-              }}
-            />
+                  backgroundColor: "#4D18E8",
+                }}
+                onClick={handleSaveButton}
+              >
+                Save
+              </Button>
+            </Stack>
+          </Box>
 
-            <Box flexGrow={1} />
+          {/* Tags Input */}
+          <Box className="flex items-center">
+            <Stack spacing={1} className="flex">
+              <Typography variant="subtitle1" className="text-[#3B354D]">
+                Tags
+              </Typography>
+              <Box
+                sx={{
+                  display: "inline-flex", // Make the Box adjust based on content size
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  gap: 0.5,
+                  padding: "0.6rem",
+                  border: "1px solid #3B354D",
+                  borderRadius: "0.5rem",
+                  backgroundColor: "#3B354D",
+                  transition: "all 0.3s ease", // Smooth transition for hover and active
+                  minWidth: "200px", // Set the minimum width for the Box
+                  maxWidth: "100%", // Let the Box expand up to 100% of its container width
+                  width: "auto", // Allow Box to take the width of its content
+                  "&:hover": {
+                    backgroundColor: "#4A435C", // Hover styles
+                    borderColor: "#A38CE6",
+                  },
+                  "&:active": {
+                    backgroundColor: "#2F283A", // Active styles
+                    borderColor: "#9B85E1",
+                  },
+                }}
+                onClick={() => document.getElementById("tags")?.focus()}
+              >
+                {tags.map((tag, index) => (
+                  <Chip
+                    key={index}
+                    label={tag}
+                    onDelete={() => handleDeleteTag(tag)}
+                    sx={{
+                      backgroundColor: "#4D18E8",
+                      color: "#E2DDF3",
+                      padding: "0.4rem",
+                      "& .MuiChip-deleteIcon": { color: "#E2DDF3" },
+                    }}
+                  />
+                ))}
+
+                <input
+                  id="tags"
+                  type="text"
+                  value={currentTag}
+                  onChange={(e) => setCurrentTag(e.target.value)}
+                  onKeyDown={handleAddTag}
+                  placeholder="Add a tag"
+                  style={{
+                    border: "none",
+                    outline: "none",
+                    background: "transparent",
+                    color: "#E2DDF3",
+                    width: "5.5rem", // Input should not be too small
+                    fontSize: "1rem", // Adjust font size as needed
+                    paddingLeft: 6, // Remove any default right padding that may create the extra space
+                    textAlign: "left", // Ensure text is aligned properly
+                  }}
+                  className="tag-input-placeholder"
+                />
+              </Box>
+            </Stack>
+          </Box>
+
+          {/* Counter for Items */}
+          <Box>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Typography variant="subtitle1">{items.length} Items</Typography>
+              <Divider className="flex-1" />
+            </Stack>
+          </Box>
+
+          {/* Upload File */}
+          <Box>
             <Button
-              variant="contained"
+              variant="outlined"
               sx={{
-                borderRadius: "0.8rem",
+                borderRadius: "0.5rem",
                 padding: "0.4rem 2rem",
                 display: "flex",
                 width: "full",
                 justifyContent: "center",
-                alignItems: "center",
-                borderColor: "#E2DDF3",
-                color: "#E2DDF3",
-                backgroundColor: "#4D18E8",
-              }}
-              onClick={handleSaveButton}
-            >
-              Save
-            </Button>
-          </Stack>
-        </Box>
-
-        {/* Tags Input */}
-        <Box className="flex items-center">
-          <Stack spacing={1} className="flex">
-            <Typography variant="subtitle1" className="text-[#3B354D]">
-              Tags
-            </Typography>
-            <Box
-              sx={{
-                display: "inline-flex", // Make the Box adjust based on content size
-                alignItems: "center",
-                flexWrap: "wrap",
-                gap: 0.5,
-                padding: "0.6rem",
-                border: "1px solid #3B354D",
-                borderRadius: "0.5rem",
-                backgroundColor: "#3B354D",
-                transition: "all 0.3s ease", // Smooth transition for hover and active
-                minWidth: "200px", // Set the minimum width for the Box
-                maxWidth: "100%", // Let the Box expand up to 100% of its container width
-                width: "auto", // Allow Box to take the width of its content
-                "&:hover": {
-                  backgroundColor: "#4A435C", // Hover styles
-                  borderColor: "#A38CE6",
-                },
-                "&:active": {
-                  backgroundColor: "#2F283A", // Active styles
-                  borderColor: "#9B85E1",
-                },
-              }}
-              onClick={() => document.getElementById("tags")?.focus()}
-            >
-              {tags.map((tag, index) => (
-                <Chip
-                  key={index}
-                  label={tag}
-                  onDelete={() => handleDeleteTag(tag)}
-                  sx={{
-                    backgroundColor: "#4D18E8",
-                    color: "#E2DDF3",
-                    padding: "0.4rem",
-                    "& .MuiChip-deleteIcon": { color: "#E2DDF3" },
-                  }}
-                />
-              ))}
-
-              <input
-                id="tags"
-                type="text"
-                value={currentTag}
-                onChange={(e) => setCurrentTag(e.target.value)}
-                onKeyDown={handleAddTag}
-                placeholder="Add a tag"
-                style={{
-                  border: "none",
-                  outline: "none",
-                  background: "transparent",
-                  color: "#E2DDF3",
-                  width: "5.5rem", // Input should not be too small
-                  fontSize: "1rem", // Adjust font size as needed
-                  paddingLeft: 6, // Remove any default right padding that may create the extra space
-                  textAlign: "left", // Ensure text is aligned properly
-                }}
-                className="tag-input-placeholder"
-              />
-            </Box>
-          </Stack>
-        </Box>
-
-        {/* Counter for Items */}
-        <Box>
-          <Stack direction="row" alignItems="center" spacing={1}>
-            <Typography variant="subtitle1">{items.length} Items</Typography>
-            <Divider className="flex-1" />
-          </Stack>
-        </Box>
-
-        {/* Upload File */}
-        <Box>
-          <Button
-            variant="outlined"
-            sx={{
-              borderRadius: "0.5rem",
-              padding: "0.4rem 2rem",
-              display: "flex",
-              width: "full",
-              justifyContent: "center",
-              color: "#3B354D",
-              border: "0.15rem solid #3B354D",
-              textTransform: "none",
-              transition: "all 0.2s ease",
-              "&:hover": {
-                borderColor: "#E2DDF3",
-                color: "#E2DDF3",
-              },
-            }}
-            onClick={handleUploadFile}
-          >
-            Upload File
-          </Button>
-        </Box>
-
-        {/* Items */}
-        <Box className="pb-6">
-          <Stack spacing={2}>
-            <AnimatePresence>
-              {items.map((item) => (
-                <motion.div
-                  key={item.id}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.5 }}
-                >
-                  <ItemComponent
-                    item={item}
-                    deleteItem={() => handleDeleteItem(item.id)}
-                    updateItem={(field, value) =>
-                      handleUpdateItem(item.id, field, value)
-                    }
-                  />
-                </motion.div>
-              ))}
-            </AnimatePresence>
-
-            <Button
-              variant="outlined"
-              sx={{
-                borderRadius: "0.6rem",
-                padding: "0.6rem 2rem",
-                display: "flex",
-                width: "full",
-                fontSize: "1rem",
-                justifyContent: "center",
                 color: "#3B354D",
                 border: "0.15rem solid #3B354D",
                 textTransform: "none",
-                bottom: 0,
                 transition: "all 0.2s ease",
                 "&:hover": {
                   borderColor: "#E2DDF3",
                   color: "#E2DDF3",
                 },
               }}
-              onClick={handleAddItem}
+              onClick={handleUploadFile}
             >
-              Add New Item
+              Upload File
             </Button>
-          </Stack>
-        </Box>
-      </Stack>
-    </Box>
+          </Box>
+
+          {/* Items */}
+          <Box className="pb-6">
+            <Stack spacing={2}>
+              <AnimatePresence>
+                {items.map((item) => (
+                  <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                  >
+                    <ItemComponent
+                      item={item}
+                      deleteItem={() => handleDeleteItem(item.id)}
+                      updateItem={(field, value) =>
+                        handleUpdateItem(item.id, field, value)
+                      }
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+
+              <Button
+                variant="outlined"
+                sx={{
+                  borderRadius: "0.6rem",
+                  padding: "0.6rem 2rem",
+                  display: "flex",
+                  width: "full",
+                  fontSize: "1rem",
+                  justifyContent: "center",
+                  color: "#3B354D",
+                  border: "0.15rem solid #3B354D",
+                  textTransform: "none",
+                  bottom: 0,
+                  transition: "all 0.2s ease",
+                  "&:hover": {
+                    borderColor: "#E2DDF3",
+                    color: "#E2DDF3",
+                  },
+                }}
+                onClick={handleAddItem}
+              >
+                Add New Item
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
+      </Box>
+    </PageTransition>
   );
 };
 
