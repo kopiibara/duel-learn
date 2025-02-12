6b72f6c3af08899f2be6397fcf41397d3fb22d88
kopiibara <jcarljames123@gmail.com>
2025-02-13 00:20:45 +0800

    merged gawa ni justine, fixes here and there

--- frontend/src/pages/user-account/SecurityCode.tsx
+++ frontend/src/pages/user-account/SecurityCode.tsx
@@ -3,6 +3,7 @@ import ExitIcon from "../../assets/images/Exit.png";
 import { useState } from "react";
 import { TextField } from "@mui/material";
 import axios from "axios"; // Ensure axios is imported
+import PageTransition from "../../styles/PageTransition";
 
 const SecurityCode = () => {
   const navigate = useNavigate();
@@ -79,103 +80,105 @@ const SecurityCode = () => {
   };
 
   return (
-    <div className="h-screen mt-[-30px] flex flex-col items-center justify-center">
-      <div className="w-[430px] sm:w-[500px] md:w-[700px] lg:w-[800px] pb-6 text-right flex justify-end">
-        <img
-          src={ExitIcon}
-          alt="Exit Icon"
-          style={{ width: "39px" }}
-          className="hover:scale-110 cursor-pointer"
-          onClick={handleExitClick}
-        />
-      </div>
+    <PageTransition>
+      <div className="h-screen mt-[-30px] flex flex-col items-center justify-center">
+        <div className="w-[430px] sm:w-[500px] md:w-[700px] lg:w-[800px] pb-6 text-right flex justify-end">
+          <img
+            src={ExitIcon}
+            alt="Exit Icon"
+            style={{ width: "39px" }}
+            className="hover:scale-110 cursor-pointer"
+            onClick={handleExitClick}
+          />
+        </div>
 
-      <div className="w-full max-w-md rounded-lg p-8 shadow-md">
-        {/* Heading */}
-        <h1 className="text-[42px] font-bold text-center text-white mb-2">
-          Enter Code
-        </h1>
-        <p className="text-[18px] text-center text-[#9F9BAE] mb-8 max-w-[340px] mx-auto break-words">
-          Please enter the security code we’ve sent to your email.{" "}
-        </p>
+        <div className="w-full max-w-md rounded-lg p-8 shadow-md">
+          {/* Heading */}
+          <h1 className="text-[42px] font-bold text-center text-white mb-2">
+            Enter Code
+          </h1>
+          <p className="text-[18px] text-center text-[#9F9BAE] mb-8 max-w-[340px] mx-auto break-words">
+            Please enter the security code we’ve sent to your email.{" "}
+          </p>
 
-        {/* Error Message Box */}
-        {error.general && (
-          <div className="w-full max-w-sm mb-4 px-4 py-2 bg-red-100 text-red-600 rounded-md border border-red-300">
-            {error.general}
-          </div>
-        )}
+          {/* Error Message Box */}
+          {error.general && (
+            <div className="w-full max-w-sm mb-4 px-4 py-2 bg-red-100 text-red-600 rounded-md border border-red-300">
+              {error.general}
+            </div>
+          )}
 
-        {/* Form */}
-        <form onSubmit={handleSubmit}>
-          {/* Email or Phone Input */}
-          <div className="mt-0 mb-0">
-            <TextField
-              id="email"
-              label="Enter code"
-              variant="filled"
-              type="text"
-              value={formData.code}
-              autoComplete="off"
-              onChange={(e) => handleInputChange("code", e.target.value)}
-              error={!!errors.code} // Apply error state if there's an error
-              sx={{
-                width: "100%",
-                backgroundColor: "#3B354D",
-                color: "#E2DDF3",
-                marginBottom: "14px",
-                borderRadius: "8px",
-                "& .MuiInputBase-root": {
-                  color: "#E2DDF3",
+          {/* Form */}
+          <form onSubmit={handleSubmit}>
+            {/* Email or Phone Input */}
+            <div className="mt-0 mb-0">
+              <TextField
+                id="email"
+                label="Enter code"
+                variant="filled"
+                type="text"
+                value={formData.code}
+                autoComplete="off"
+                onChange={(e) => handleInputChange("code", e.target.value)}
+                error={!!errors.code} // Apply error state if there's an error
+                sx={{
+                  width: "100%",
                   backgroundColor: "#3B354D",
+                  color: "#E2DDF3",
+                  marginBottom: "14px",
                   borderRadius: "8px",
-                  "&:hover": {
+                  "& .MuiInputBase-root": {
+                    color: "#E2DDF3",
                     backgroundColor: "#3B354D",
+                    borderRadius: "8px",
+                    "&:hover": {
+                      backgroundColor: "#3B354D",
+                    },
+                    "&.Mui-focused": {
+                      backgroundColor: "#3B354D",
+                    },
                   },
-                  "&.Mui-focused": {
-                    backgroundColor: "#3B354D",
+                  "& .MuiInputLabel-root": {
+                    color: "#9F9BAE",
                   },
-                },
-                "& .MuiInputLabel-root": {
-                  color: "#9F9BAE",
-                },
-                "& .MuiInput-underline:before": {
-                  borderBottomColor: "#9F9BAE",
-                },
-                "& .MuiInput-underline:after": {
-                  borderBottomColor: "#4D18E8",
-                },
-                // Apply error styles if there's an error
-                "& .MuiFilledInput-root": {
-                  borderColor: errors.code ? "red" : "#9F9BAE", // Border color when there's an error
-                  "&:hover": {
-                    borderColor: errors.code ? "red" : "#9F9BAE", // Border color on hover when there's an error
+                  "& .MuiInput-underline:before": {
+                    borderBottomColor: "#9F9BAE",
                   },
-                },
-              }}
-            />
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "#4D18E8",
+                  },
+                  // Apply error styles if there's an error
+                  "& .MuiFilledInput-root": {
+                    borderColor: errors.code ? "red" : "#9F9BAE", // Border color when there's an error
+                    "&:hover": {
+                      borderColor: errors.code ? "red" : "#9F9BAE", // Border color on hover when there's an error
+                    },
+                  },
+                }}
+              />
 
-            {errors.code && (
-              <div className="text-red-500 mb-3 text-sm mt-[-9px]">
-                {errors.code}
-              </div>
-            )}
-          </div>
+              {errors.code && (
+                <div className="text-red-500 mb-3 text-sm mt-[-9px]">
+                  {errors.code}
+                </div>
+              )}
+            </div>
 
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-full mt-2 bg-[#4D18E8] text-white py-3 rounded-lg hover:bg-[#6931E0] transition-colors flex justify-center items-center"
-          >
-            {buttonLoading ? (
-              <div className="loader border-2 border-t-2 border-white rounded-full w-6 h-6 animate-spin"></div>
-            ) : (
-              "Verify"
-            )}
-          </button>
-        </form>
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="w-full mt-2 bg-[#4D18E8] text-white py-3 rounded-lg hover:bg-[#6931E0] transition-colors flex justify-center items-center"
+            >
+              {buttonLoading ? (
+                <div className="loader border-2 border-t-2 border-white rounded-full w-6 h-6 animate-spin"></div>
+              ) : (
+                "Verify"
+              )}
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </PageTransition>
   );
 };
 
