4b8984e7b577ddc740dd1f89f656e2b25a4e278d
jing388 <uy.gwynethfabul@gmail.com>
2025-02-17 15:48:45 +0800

    fix styling here and there

--- frontend/src/pages/user-account/CheckYourMail.tsx
+++ frontend/src/pages/user-account/CheckYourMail.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import EmailSent from '../../assets/General/EmailSent.png'; // Importing the big star image
-import PageTransition from '../../styles/PageTransition'; // Importing the PageTransition component
+import { Link, useNavigate } from "react-router-dom";
+import EmailSent from "../../assets/General/EmailSent.png"; // Importing the big star image
+import PageTransition from "../../styles/PageTransition"; // Importing the PageTransition component
 
 export default function CheckYourMail() {
   const [countdown, setCountdown] = useState<number | null>(null); // State to manage countdown timer
@@ -26,20 +26,16 @@ export default function CheckYourMail() {
         // Main container with flexbox layout, padding, and background color
       >
         <div className="flex flex-col max-w-full w-[573px]">
-          {/* Container for the content with flexbox layout and specific width */}
-          <header className="flex gap-3 self-start text-xl font-bold text-white absolute top-12 left-10 max-md:left-2">
-            {/* Header section with flexbox layout, gap, and text styling */}
-            <img
-              loading="lazy"
-              src="/duel-learn-logo.svg"
-              className="object-contain shrink-0 aspect-square w-[37px] h-[37px]"
-              alt="Duel Learn Logo"
-              // Importing Duel Learn logo in SVG format
-            />
-            <h1 className="my-auto basis-auto" style={{ fontFamily: "Nunito" }}>
-              {/* Title with specific styling */}
-              Duel Learn
-            </h1>
+          {/* Simple Header */}
+          <header className="absolute top-20 left-20 flex items-center">
+            <Link to="/" className="flex items-center space-x-4">
+              <img
+                src="/duel-learn-logo.svg"
+                className="w-10 h-10"
+                alt="icon"
+              />
+              <p className="text-white text-xl font-semibold">Duel Learn</p>
+            </Link>
           </header>
 
           <section className="flex flex-col items-center mt-14 ml-40 max-w-full text-center w-[213px] max-md:mt-5 max-md:ml-5">
@@ -53,14 +49,20 @@ export default function CheckYourMail() {
             />
             <div className="flex flex-col items-center mt-3 max-md:mt-4 max-md:max-w-full w-[400px]">
               {/* Container for the text content with flexbox layout and margin */}
-              <h2 className="text-4xl font-bold text-slate-200 max-md:max-w-full max-md:text-3xl mb-2" style={{ fontFamily: "Nunito" }}>
+              <h2
+                className="text-4xl font-bold text-slate-200 max-md:max-w-full max-md:text-3xl mb-2"
+                style={{ fontFamily: "Nunito" }}
+              >
                 {/* Heading with specific font size and color */}
                 Check your mailbox!
               </h2>
-              <p className="mt-2 text-m text-zinc-400 max-md:max-w-full mb-2" style={{ fontFamily: "Nunito" }}>
+              <p
+                className="mt-2 text-m text-zinc-400 max-md:max-w-full mb-2"
+                style={{ fontFamily: "Nunito" }}
+              >
                 {/* Paragraph with margin, font size, and color */}
-                We sent you a link for your password recovery. Check your spam folder
-                if you do not hear from us after awhile.
+                We sent you a link for your password recovery. Check your spam
+                folder if you do not hear from us after awhile.
               </p>
             </div>
 
@@ -70,7 +72,7 @@ export default function CheckYourMail() {
                 {/* Container for the button with flexbox layout */}
                 <button
                   className="w-[400px] px-5 py-2 bg-violet-700 rounded-xl max-md:px-2 max-md:max-w-full hover:bg-violet-600 transition-colors text-base"
-                  onClick={() => navigate('/sign-up')}
+                  onClick={() => navigate("/sign-up")}
                   style={{ fontFamily: "Nunito" }}
                   // Button with specific styling and click event to navigate to sign-up page
                 >
