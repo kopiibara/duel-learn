9f6204e672d29e926980ea7d4427ae12f4d019f7
Bea Alyssa Lugtu <149260052+BeaLugtu@users.noreply.github.com>
2025-02-21 22:40:08 +0800

    added Home Banner

--- frontend/src/pages/dashboard/home/HomePage.tsx
+++ frontend/src/pages/dashboard/home/HomePage.tsx
@@ -6,6 +6,8 @@ import PageTransition from "../../../styles/PageTransition";
 import ChooseYourChallenge from "../../../components/ChooseYourChallenge";
 import DiscoverMore from "./DiscoverMore";
 import RecentlyOpened from "./RecentlyOpened";
+import PremiumAdsBG from "../../../assets/shop-picture/premium-ads-bg.png";
+
 
 const HomePage = ({
   setSelectedIndex,
@@ -23,6 +25,28 @@ const HomePage = ({
     <PageTransition>
       <Box className="h-screen w-full">
         <DocumentHead title="Home | Duel Learn" />
+        <div
+          className="h-[232px] mx-8 rounded-lg p-6 px-12 flex flex-col justify-center items-start text-left mb-10"
+          style={{
+            background: "linear-gradient(90deg, #9F87E5 0%, #6F58D9 100%)",
+          }}
+        >
+            <h1
+              className="text-white text-xl md:text-[29px] font-medium mb-2"
+              style={{ letterSpacing: "0.01em", lineHeight: "1.1" }}
+            >
+              Those gaps in your <br /> materials? Let’s fill ‘em up!
+            </h1>
+          <p className="text-white text-sm md:text-[15px] mt-1 opacity-80">
+            AI cross-referencing available in Duel-Learn Premium!
+          </p>
+          <button
+            className="mt-4 px-6 py-2 text-sm md:text-base bg-white text-[#9F87E5] rounded-full font-bold"
+            onClick={() => navigate("/dashboard/buy-premium-account")}
+          >
+            Learn More
+          </button>
+        </div>
         <Stack spacing={2} className="px-6">
           <Stack spacing={2} className="pb-6">
             <Stack
