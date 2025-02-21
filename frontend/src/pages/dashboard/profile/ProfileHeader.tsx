6aa9943575657ac7f791f71f3a243e748a94e2e1
Bea Alyssa Lugtu <149260052+BeaLugtu@users.noreply.github.com>
2025-02-21 22:21:37 +0800

    updated responsiveness to the profile avatar and lb

--- frontend/src/pages/dashboard/profile/ProfileHeader.tsx
+++ frontend/src/pages/dashboard/profile/ProfileHeader.tsx
@@ -27,54 +27,57 @@ const ProfileHeader = () => {
   }, [profileData.xp, profileData.xpRequired]);
 
   return (
-    <div
-      className="rounded-lg px-14 py-9 flex items-center mb-9"
-      style={{
-        backgroundColor: "rgba(61, 56, 76, 0.25)",
-        border: "1px solid rgba(61, 56, 76, 0.25)",
-      }}
-    >
-      {/* Avatar */}
-      <div className="mr-4">
-        <img
-          src={Profile}
-          alt="Profile Avatar"
-          className="w-[158px] h-[158px] rounded-sm mr-6"
-        />
-      </div>
+<div
+  className="rounded-lg px-6 md:px-14 py-9 flex flex-col md:flex-row items-center md:items-start text-center md:text-left mb-9"
+  style={{
+    backgroundColor: "rgba(61, 56, 76, 0.25)",
+    border: "1px solid rgba(61, 56, 76, 0.25)",
+  }}
+>
+  {/* Avatar */}
+  <div className="sm:self-center md:self-start mb-6 md:mb-0 md:mr-6">
+    <img
+      src={Profile}
+      alt="Profile Avatar"
+      className="w-[158px] h-[158px] rounded-sm"
+    />
+  </div>
 
-      {/* Profile Info */}
-      <div className="flex-1">
-        <div className="flex items-center">
-          <h2 className="text-[30px] font-bold mr-3">{profileData.username}</h2>
-          <img
-            src={PremiumLabel}
-            alt="Profile Premium Icon"
-            className="w-[15px] h-[auto] rounded-sm mr-6"
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <p className="text-[#6F658D] text-[18px] mt-1">
-            LVL {profileData.level}
-          </p>
-          <p className="text-[#6F658D] text-[13px] mt-1">
-            {profileData.xp} / {profileData.xpRequired} XP
-          </p>
-        </div>
-
-        {/* XP Progress Bar */}
-        <div className="mb-4 relative w-full bg-[#3E3E50] h-[6px] rounded-full mt-2">
-          <div
-            className="absolute top-0 left-0 h-[6px] bg-[#2B00FF] rounded-full"
-            style={{ width: `${profileData.xpProgress}%` }}
-          ></div>
-        </div>
-        {/* Friends */}
-        <p className="text-[#6F658D] text-[18px] mt-2">
-          {profileData.friendsCount} Friends
-        </p>
-      </div>
+  {/* Profile Info */}
+  <div className="flex-1">
+    <div className="flex justify-center md:justify-start items-center">
+      <h2 className="text-[30px] font-bold mr-3">{profileData.username}</h2>
+      <img
+        src={PremiumLabel}
+        alt="Profile Premium Icon"
+        className="w-[15px] h-auto rounded-sm"
+      />
     </div>
+    <div className="flex justify-center md:justify-between">
+      <p className="text-[#6F658D] text-[18px] mt-1">
+        LVL {profileData.level}
+      </p>
+      <p className="text-[#6F658D] text-[13px] mt-1 md:ml-2">
+        {profileData.xp} / {profileData.xpRequired} XP
+      </p>
+    </div>
+
+    {/* XP Progress Bar */}
+    <div className="relative w-full bg-[#3E3E50] h-[6px] rounded-full mt-2">
+      <div
+        className="absolute top-0 left-0 h-[6px] bg-[#2B00FF] rounded-full"
+        style={{ width: `${profileData.xpProgress}%` }}
+      ></div>
+    </div>
+
+    {/* Friends */}
+    <p className="text-[#6F658D] text-[18px] mt-2">
+      {profileData.friendsCount} Friends
+    </p>
+  </div>
+</div>
+
+
   );
 };
 
