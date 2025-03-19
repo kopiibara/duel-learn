dc036ca1ed4d4e921fee61ee092f3ba65caaf047
kopiibara <jcarljames123@gmail.com>
2025-03-19 21:10:46 +0800

    fixed dotenv

--- backend/services/firebaseAdmin.js
+++ backend/services/firebaseAdmin.js
@@ -1,11 +1,21 @@
+import dotenv from "dotenv";
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 
-const serviceAccount = JSON.parse(readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
+dotenv.config();
+
+
+const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+if (!credentialsPath) {
+  throw new Error("GOOGLE_APPLICATION_CREDENTIALS is not defined in environment variables.");
+}
+
+const serviceAccount = JSON.parse(readFileSync(credentialsPath, 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://duel-learn-default-rtdb.firebaseio.com/'
+  databaseURL: 'https://duel-learn-default-rtdb.firebaseio.com/',
 });
 
 export default admin;
