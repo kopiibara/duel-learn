1172cf3e96b5851f199eb1c45106a284357a7e76
Lair027 <samislaircoleenw@gmail.com>
2025-03-28 21:05:07 +0800

    modify the import location

--- backend/routes/sessionReport.js
+++ backend/routes/sessionReport.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { saveSessionReport } from '../controllers/SessionReportController.js';
+import { saveSessionReport } from '../controller/SessionReportController.js';
 
 const router = express.Router();
 
